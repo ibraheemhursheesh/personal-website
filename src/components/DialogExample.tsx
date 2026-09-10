b03608020c978425eab59087da42191f510db041
ibrahimharchiche <ibrahimharchiche44@gmail.com>
2026-09-10 11:18:34 +0100

    remove textarea

--- src/components/DialogExample.tsx
+++ src/components/DialogExample.tsx
@@ -29,7 +29,6 @@ export default function DialogExample() {
         closedby="any"
         className="m-auto w-full max-w-[calc(100%-2rem)] gap-4 rounded-xl border-0 bg-popover p-4 text-sm text-popover-foreground ring-1 ring-foreground/10 outline-none open:grid backdrop:bg-black/10 backdrop:backdrop-blur-xs sm:max-w-sm"
       >
-        <textarea className="border-3" />
         <button
           type="button"
           aria-label="Close"
